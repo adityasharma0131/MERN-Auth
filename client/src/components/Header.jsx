3a458141a1860af8fafc3e4739986b55ddb9fe47
adityasharma0131 <adityasharma0431@gmail.com>
2024-06-02 21:24:39 +0530

    navbar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,30 +1,77 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
-const Header = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
+export default function NavBar() {
+  const [navbar, setNavbar] = useState(false);
 
   return (
-    <div className="bg-gray-900 text-white">
-      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
-        <h1 className="font-bold text-xl">MERN-Auth</h1>
-        <button
-          className="md:hidden text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-gray-600"
-          onClick={toggleMenu}
-        >
-          <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}></path>
-          </svg>
-        </button>
-        <ul className={`flex-col md:flex-row md:flex gap-4 absolute md:static top-12 left-0 w-full md:w-auto bg-gray-900 md:bg-transparent p-4 md:p-0 ${isOpen ? 'flex' : 'hidden'}`}>
-          <li className=" flex justify-center items-center p-2 md:py-0"><a href="#" className=" py-2 px-2 hover:text-blue-400 hover:rounded-sm hover:bg-gray-600">Home</a></li>
-        </ul>
+    <nav className=" sticky top-0 z-10 bg-white shadow backdrop-filter backdrop-blur-lg bg-opacity-30  border-b border-gray-200">
+      <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
+        <div>
+          <div className="flex items-center justify-between py-3 md:py-5 md:block">
+            <a href="javascript:void(0)">
+              <h2 className="text-2xl font-bold">MERN-Auth</h2>
+            </a>
+            <div className="md:hidden">
+              <button
+                className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                onClick={() => setNavbar(!navbar)}
+              >
+                {navbar ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div
+            className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+              navbar ? "block" : "hidden"
+            }`}
+          >
+            <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+              <li className="text-gray-600 hover:text-blue-600">
+                <a href="javascript:void(0)">Home</a>
+              </li>
+              <li className="text-gray-600 hover:text-blue-600">
+                <a href="javascript:void(0)">Blog</a>
+              </li>
+              <li className="text-gray-600 hover:text-blue-600">
+                <a href="javascript:void(0)">About US</a>
+              </li>
+              <li className="text-gray-600 hover:text-blue-600">
+                <a href="javascript:void(0)">Contact US</a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
-    </div>
+    </nav>
   );
-};
-
-export default Header;
+}
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,95 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur
+      vitae numquam aperiam magnam eum, animi culpa architecto facere ducimus
+      eveniet necessitatibus tempore aspernatur quod minima, explicabo harum
+      unde ad aliquid? Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      Voluptates, voluptatem aliquam? Perspiciatis dolorum incidunt, iste
+      similique quo, eligendi quae corporis magni eveniet illo repudiandae
+      quaerat quos quasi ipsam. Non, ipsa. Lorem ipsum dolor sit amet
+      consectetur, adipisicing elit. Consequatur vitae numquam aperiam magnam
+      eum, animi culpa architecto facere ducimus eveniet necessitatibus tempore
+      aspernatur quod minima, explicabo harum unde ad aliquid? Lorem ipsum dolor
+      sit amet, consectetur adipisicing elit. Voluptates, voluptatem aliquam?
+      Perspiciatis dolorum incidunt, iste similique quo, eligendi quae corporis
+      magni eveniet illo repudiandae quaerat quos quasi ipsam. Non, ipsa. Lorem
+      ipsum dolor sit amet consectetur, adipisicing elit. Consequatur vitae
+      numquam aperiam magnam eum, animi culpa architecto facere ducimus eveniet
+      necessitatibus tempore aspernatur quod minima, explicabo harum unde ad
+      aliquid? Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      Voluptates, voluptatem aliquam? Perspiciatis dolorum incidunt, iste
+      similique quo, eligendi quae corporis magni eveniet illo repudiandae
+      quaerat quos quasi ipsam. Non, ipsa. Lorem ipsum dolor sit amet
+      consectetur, adipisicing elit. Consequatur vitae numquam aperiam magnam
+      eum, animi culpa architecto facere ducimus eveniet necessitatibus tempore
+      aspernatur quod minima, explicabo harum unde ad aliquid? Lorem ipsum dolor
+      sit amet consectetur, adipisicing elit. Consequatur vitae numquam aperiam
+      magnam eum, animi culpa architecto facere ducimus eveniet necessitatibus
+      tempore aspernatur quod minima, explicabo harum unde ad aliquid? Lorem
+      ipsum dolor sit amet consectetur, adipisicing elit. Consequatur vitae
+      numquam aperiam magnam eum, animi culpa architecto facere ducimus eveniet
+      necessitatibus tempore aspernatur quod minima, explicabo harum unde ad
+      aliquid? Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      Consequatur vitae numquam aperiam magnam eum, animi culpa architecto
+      Consequatur vitae numquam aperiam magnam eum, animi culpa architecto
+      Consequatur vitae numquam aperiam magnam eum, animi culpa architecto
+      Consequatur vitae numquam aperiam magnam eum, animi culpa architecto
+      Consequatur vitae numquam aperiam magnam eum, animi culpa architecto
+      Consequatur vitae numquam aperiam magnam eum, animi culpa architecto
+      Consequatur vitae numquam aperiam magnam eum, animi culpa architecto
+      Consequatur vitae numquam aperiam magnam eum, animi culpa architecto
+      Consequatur vitae numquam aperiam magnam eum, animi culpa architecto
+      Consequatur vitae numquam aperiam magnam eum, animi culpa architecto
+      Consequatur vitae numquam aperiam magnam eum, animi culpa architecto
+      Consequatur vitae numquam aperiam magnam eum, animi culpa architecto
+      Consequatur vitae numquam aperiam magnam eum, animi culpa architecto
+      Consequatur vitae numquam aperiam magnam eum, animi culpa architecto
+      Consequatur vitae numquam aperiam magnam eum, animi culpa architecto
+      Consequatur vitae numquam aperiam magnam eum, animi culpa architecto
+      Consequatur vitae numquam aperiam magnam eum, animi culpa architecto
+      Consequatur vitae numquam aperiam magnam eum, animi culpa architecto
+      Consequatur vitae numquam aperiam magnam eum, animi culpa architecto
+      Consequatur vitae numquam aperiam magnam eum, animi culpa architecto
+      Consequatur vitae numquam aperiam magnam eum, animi culpa architecto
+      Consequatur vitae numquam aperiam magnam eum, animi culpa architecto
+      Consequatur vitae numquam aperiam magnam eum, animi culpa architecto
+      Consequatur vitae numquam aperiam magnam eum, animi culpa architecto
+      Consequatur vitae numquam aperiam magnam eum, animi culpa architecto
+      Consequatur vitae numquam aperiam magnam eum, animi culpa architecto
+      Consequatur vitae numquam aperiam magnam eum, animi culpa architecto
+      facere ducimus eveniet necessitatibus tempore aspernatur quod minima,
+      explicabo harum unde ad aliquid? Lorem ipsum dolor sit amet consectetur,
+      adipisicing elit. Consequatur vitae numquam aperiam magnam eum, animi
+      culpa architecto facere ducimus eveniet necessitatibus tempore aspernatur
+      quod minima, explicabo harum unde ad aliquid? Lorem ipsum dolor sit amet
+      consectetur, adipisicing elit. Consequatur vitae numquam aperiam magnam
+      eum, animi culpa architecto facere ducimus eveniet necessitatibus tempore
+      aspernatur quod minima, explicabo harum unde ad aliquid? Lorem ipsum dolor
+      sit amet consectetur, adipisicing elit. Consequatur vitae numquam aperiam
+      magnam eum, animi culpa architecto facere ducimus eveniet necessitatibus
+      tempore aspernatur quod minima, explicabo harum unde ad aliquid? Lorem
+      ipsum dolor sit amet, consectetur adipisicing elit. Voluptates, voluptatem
+      aliquam? Perspiciatis dolorum incidunt, iste similique quo, eligendi quae
+      corporis magni eveniet illo repudiandae quaerat quos quasi ipsam. Non,
+      ipsa. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      Consequatur vitae numquam aperiam magnam eum, animi culpa architecto
+      facere ducimus eveniet necessitatibus tempore aspernatur quod minima,
+      explicabo harum unde ad aliquid? Lorem ipsum dolor sit amet, consectetur
+      adipisicing elit. Voluptates, voluptatem aliquam? Perspiciatis dolorum
+      incidunt, iste similique quo, eligendi quae corporis magni eveniet illo
+      repudiandae quaerat quos quasi ipsam. Non, ipsa. Lorem ipsum dolor sit
+      amet consectetur, adipisicing elit. Consequatur vitae numquam aperiam
+      magnam eum, animi culpa architecto facere ducimus eveniet necessitatibus
+      tempore aspernatur quod minima, explicabo harum unde ad aliquid? Lorem
+      ipsum dolor sit amet, consectetur adipisicing elit. Voluptates, voluptatem
+      aliquam? Perspiciatis dolorum incidunt, iste similique quo, eligendi quae
+      corporis magni eveniet illo repudiandae quaerat quos quasi ipsam. Non,
+      ipsa.
+    </div>
+  );
+};
 
-export default Home
+export default Home;
