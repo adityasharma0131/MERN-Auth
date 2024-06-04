1dd096240da3f83d6728c774eec92b1f55c9a689
adityasharma0131 <adityasharma0431@gmail.com>
2024-06-04 18:33:12 +0530

    validatinng if the user aldready exists or not

--- server/routes/signup.js
+++ server/routes/signup.js
@@ -1,12 +1,25 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 const User = require("./DB");
 
 router.post("/", async function (req, res, next) {
   const { username, email, password } = req.body;
-  const newUser = new User({ username, email, password });
-  await newUser.save();
-  res.status(201).json({ message: "User Created Successfully!" });
+
+  try {
+    // Check if a user with the same username or email already exists
+    const existingUser = await User.findOne({ $or: [{ username }, { email }] });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const newUser = new User({ username, email, password });
+
+    // Save the new user to the database
+    await newUser.save();
+    res.status(201).json({ message: "User Created Successfully!" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 module.exports = router;
